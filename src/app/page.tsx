a14d9bf71f90f332eb8d3d5bad6c71732d43d10a
nelsonoftrafalgar <nelsonoftrafalgar@gmail.com>
2025-04-21 22:10:44 +0200

    h1 title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,18 +4,21 @@ import { Matrix } from '@/components/Matrix'
 
 export default function Home() {
 	return (
-		<div className='master'>
-			<div className='important'>IMPORTANT</div>
-			<div className='urgent'>
-				<span>URGENT</span>
+		<>
+			<h1 className='title'>EISENHOWER MATRIX</h1>
+			<div className='master'>
+				<div className='important'>IMPORTANT</div>
+				<div className='urgent'>
+					<span>URGENT</span>
+				</div>
+				<div className='grid'>
+					<Matrix />
+				</div>
+				<div className='not-urgent'>
+					<span>NOT URGENT</span>
+				</div>
+				<div className='not-important'>NOT IMPORTANT</div>
 			</div>
-			<div className='grid'>
-				<Matrix />
-			</div>
-			<div className='not-urgent'>
-				<span>NOT URGENT</span>
-			</div>
-			<div className='not-important'>NOT IMPORTANT</div>
-		</div>
+		</>
 	)
 }
